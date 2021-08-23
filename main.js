a954838dec2a76dd7492d81e083c440fe51d905c
tennis-momentum <78315679+jirung27@users.noreply.github.com>
2021-08-23 12:20:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,5 @@
-$(document).ready(function(){             
+//지도를 삽입할 HTML 요소 또는 HTML 요소의 id를 지정합니다.
+var mapDiv = document.getElementById('map'); // 'map'으로 선언해도 동일
 
- 
-
-  var selectTarget = $('.selectbox select');
-  
-   
-  
-  selectTarget.on('blur', function(){
-  
-  $(this).parent().removeClass('focus');
-  
-  });
-  
-   
-  
-  selectTarget.change(function(){
-  
-  var select_name =
-  
-  $(this).children('option:selected').text();
-  
-   
-  
-  $(this).siblings('label').text(select_name);
-  
-  });
-  
-  });
-  
-   
-  
-   
+//옵션 없이 지도 객체를 생성하면 서울 시청을 중심으로 하는 16 레벨의 지도가 생성됩니다.
+var map = new naver.maps.Map(mapDiv);
